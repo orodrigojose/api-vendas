c140d6f3b3d0f21e52702ef723e36d4a18bc3bf1
rsx <contato.rodrigosilv@gmail.com>
2024-04-29 09:48:59 -0300

    feat: setup typeorm classroom 16

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,32 @@
-import express from 'express';
+import "reflect-metadata"
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
 import cors from 'cors';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 const PORT = 3333;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    // if error is equal apperror (your structure and keys and name and more than more)
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    // else return an generic message error
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server Error."
+    })
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`[!] server started on port ${PORT}`);
